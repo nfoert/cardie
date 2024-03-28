d9ac8ff8caaabcc617338e4295707e7a419552a8
nfoert <nofoert@gmail.com>
2024-03-28 16:10:59 -0400

    - Design styles for button UI elements - Add a ui.html for user interface element testing - Remove some accadentally commited pycache files

--- cardie/main/urls.py
+++ cardie/main/urls.py
@@ -4,4 +4,5 @@ from . import views
 
 urlpatterns = [
     path("", views.index, name="index"),
+    path("ui", views.userinterface, name="ui"),
 ]
--- cardie/main/views.py
+++ cardie/main/views.py
@@ -2,3 +2,6 @@ from django.shortcuts import HttpResponse, render
 
 def index(request):
     return render(request, "index.html")
+
+def userinterface(request):
+    return render(request, "ui.html")

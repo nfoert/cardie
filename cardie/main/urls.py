1d1df493602b99849b51c19b9051ec0fff781b1a
nfoert <nofoert@gmail.com>
2024-04-23 16:18:15 -0400

    Add authentication page

--- cardie/main/urls.py
+++ cardie/main/urls.py
@@ -4,5 +4,6 @@ from . import views
 
 urlpatterns = [
     path("", views.index, name="index"),
+    path("authentication", views.authentication, name="authentication"),
     path("ui", views.userinterface, name="ui"),
 ]
--- cardie/main/views.py
+++ cardie/main/views.py
@@ -3,5 +3,8 @@ from django.shortcuts import HttpResponse, render
 def index(request):
     return render(request, "index.html")
 
+def authentication(request):
+    return render(request, "authentication.html")
+
 def userinterface(request):
     return render(request, "ui.html")

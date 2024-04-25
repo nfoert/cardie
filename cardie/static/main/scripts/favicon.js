2f5a1e1bb7f9f340c4623e3476103d1635f2f3d4
nfoert <nofoert@gmail.com>
2024-04-25 16:04:15 -0400

    Favicon system based on prefers-color-scheme

---
+++ cardie/static/main/scripts/favicon.js
@@ -0,0 +1,47 @@
+// Thanks to https://stackoverflow.com/questions/260857/changing-website-favicon-dynamically
+
+let favicon_light_url = "http://127.0.0.1:8000/static/main/images/favicon_light.ico";
+let favicon_dark_url = "http://127.0.0.1:8000/static/main/images/favicon_dark.ico"
+
+function favicon_light() {
+    var link = document.querySelector("link[rel~='icon']");
+
+    if (!link) {
+        link = document.createElement('link');
+        link.rel = 'icon';
+        document.head.appendChild(link);
+    }
+
+    link.href = favicon_light_url;
+}
+
+function favicon_dark() {
+    var link = document.querySelector("link[rel~='icon']");
+
+    if (!link) {
+        link = document.createElement('link');
+        link.rel = 'icon';
+        document.head.appendChild(link);
+    }
+
+    link.href = favicon_dark_url;
+}
+
+if (window.matchMedia && window.matchMedia('(prefers-color-scheme: dark)').matches) {
+    favicon_light();
+    
+} else {
+    favicon_dark();
+}
+
+window.matchMedia('(prefers-color-scheme: dark)').addEventListener('change', event => {
+    const newColorScheme = event.matches ? "dark" : "light";
+    
+    if (newColorScheme == "dark") {
+        favicon_light();
+
+    } else {
+        favicon_dark();
+    }
+});
+

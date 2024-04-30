ba3924665cf60a40eaaf613967dbb30c039dec8e
nfoert <nofoert@gmail.com>
2024-04-30 18:28:52 -0400

    Card models

--- cardie/main/admin.py
+++ cardie/main/admin.py
@@ -1,6 +1,7 @@
 from django.contrib import admin
 from authentication.models import User
-from main.models import Server
+from main.models import Server, Card
 
 admin.site.register(User)
 admin.site.register(Server)
+admin.site.register(Card)
--- cardie/main/models.py
+++ cardie/main/models.py
@@ -1,4 +1,6 @@
+from colorfield.fields import ColorField
 from django.db import models
+from authentication.models import User
 
 class Server(models.Model):
     ip = models.CharField(max_length=256)
@@ -6,3 +8,23 @@ class Server(models.Model):
 
     def __str__(self):
         return(self.ip)
+
+class Card(models.Model):
+    layout_choices = {
+        "LE": "Left",
+        "RI": "Right",
+        "CE": "Center"
+    }
+
+    name = models.CharField(max_length=256)
+    owner = models.ForeignKey(User, on_delete=models.CASCADE)
+    layout = models.CharField(choices=layout_choices, max_length=128)
+    user_name = models.CharField(max_length=128)
+    user_pronouns = models.CharField(max_length=128)
+    text_info = models.JSONField(default=dict, blank=True)
+    link_info = models.JSONField(default=dict, blank=True)
+    background_color = ColorField(default="#FF0000")
+
+    def __str__(self):
+        return(f"{self.owner.username}'s Card, '{self.name}'")
+

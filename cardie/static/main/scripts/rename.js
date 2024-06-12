95ae3dd574320561262e65d8a5de255df61bbdac
nfoert <nofoert@gmail.com>
2024-06-11 21:17:39 -0400

    Basic rename box functionality

--- cardie/static/main/scripts/rename.js
+++ cardie/static/main/scripts/rename.js
@@ -16,3 +16,22 @@ function hide_rename() {
         hide_background_blur();
     }, 500);
 }
+
+function rename_card() {
+    let card_name = document.querySelector("#editor_rename_input").value;
+
+    document.querySelector("#editor_header_name_text_cardname").innerText = card_name;
+    hide_rename();
+}
+
+document.querySelector("#editor_rename_top_close").addEventListener("click", (event) => {
+    hide_rename();
+});
+
+document.querySelector("#editor_rename_submit").addEventListener("click", (event) => {
+    rename_card();
+});
+
+document.querySelector("#editor_header_name_button").addEventListener("click", (event) => {
+    show_rename();
+});

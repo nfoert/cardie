dda0d6674fd3ac48149cfd437e1d1fea09a24a90
nfoert <nofoert@gmail.com>
2024-07-02 18:20:28 -0400

    Production server settings

--- cardie/cardie/wsgi.py
+++ cardie/cardie/wsgi.py
@@ -11,6 +11,6 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'cardie.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'cardie.settings_development')
 
 application = get_wsgi_application()

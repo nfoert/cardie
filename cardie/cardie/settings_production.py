b5cbb134cd42ff190900e73d930fa83d48b09d98
nfoert <nofoert@gmail.com>
2024-07-02 22:27:01 -0400

    Set ALLOWED_HOSTS to be a list

--- cardie/cardie/settings_production.py
+++ cardie/cardie/settings_production.py
@@ -26,7 +26,7 @@ SECRET_KEY = os.environ.get("SECRET_KEY")
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = os.environ.get("DEBUG")
 
-ALLOWED_HOSTS = os.environ.get("ALLOWED_HOSTS")
+ALLOWED_HOSTS = list(os.environ.get("ALLOWED_HOSTS"))
 
 
 # Application definition

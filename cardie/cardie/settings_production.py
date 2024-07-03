b9f5732f69577bf809cb3f91aa47f12f43a8d5df
nfoert <nofoert@gmail.com>
2024-07-03 10:37:04 -0400

    Install dj-database-url

--- cardie/cardie/settings_production.py
+++ cardie/cardie/settings_production.py
@@ -12,6 +12,7 @@ https://docs.djangoproject.com/en/5.0/ref/settings/
 
 from pathlib import Path
 import os
+import dj_database_url
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent

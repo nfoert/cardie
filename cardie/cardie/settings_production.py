2f79de24bc9d8447b3f4bd5b9aba9a288023526a
nfoert <nofoert@gmail.com>
2024-07-03 09:55:44 -0400

    change name of ALLOWED_HOSTS environment variable

--- cardie/cardie/settings_production.py
+++ cardie/cardie/settings_production.py
@@ -26,7 +26,7 @@ SECRET_KEY = os.environ.get("SECRET_KEY")
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = os.getenv("DEBUG", "False") == "True"
 
-ALLOWED_HOSTS = os.getenv("ALLOWED_HOSTS", "127.0.0.1,localhost").split(",")
+ALLOWED_HOSTS = os.getenv("DJANGO_ALLOWED_HOSTS", "127.0.0.1,localhost").split(",")
 
 # Application definition
 

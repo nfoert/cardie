6aac82e9c89f67d13bbbe20bd8b3f577ada687c6
nfoert <nofoert@gmail.com>
2024-10-07 20:28:33 -0400

    bug: Fix #82 Context processors prevented an initial server object from being created

--- cardie/main/context_processors.py
+++ cardie/main/context_processors.py
@@ -1,17 +1,27 @@
 from django.core.exceptions import ImproperlyConfigured
+from django.conf import settings
 
 from .models import Server
 
 def server_context(request):
     server_context = Server.objects.first()
-    if server_context is None:
-        raise ImproperlyConfigured(
-            "No Server object found in the database. Tip: please add one."
-        )
+    if settings.ADMIN_PATH not in request.path: 
+        if server_context is None:
+            raise ImproperlyConfigured(
+                "No Server object found in the database. Tip: please add one."
+            )
 
-    return {
-        "server": {
-            "ip": server_context.ip,
-            "production": server_context.production,
+        return {
+            "server": {
+                "ip": server_context.ip,
+                "production": server_context.production
+            }
         }
-    }
+
+    else:
+        return {
+            "server": {
+                "ip": "",
+                "production": ""
+            }
+        }

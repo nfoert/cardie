0abc4053559da471f639a8e0974cd32e4b9c9e2e
madman <hrishikesh0246@gmail.com>
2024-10-23 21:30:16 +0530

    added max image size

--- cardie/authentication/forms.py
+++ cardie/authentication/forms.py
@@ -1,6 +1,6 @@
 from django import forms
 from .models import Profile, User
-
+from django.core.exceptions import ValidationError
 
 
 class UserUpdateForm(forms.ModelForm):
@@ -17,3 +17,14 @@ class ProfileUpdateForm(forms.ModelForm):
     class Meta:
         model = Profile
         fields = ['profile_image', 'bio']
+
+    def clean_profile_image(self):
+        image = self.cleaned_data.get('profile_image', False)
+        if image:
+            if image.size > 2 * 1024 * 1024:
+                raise ValidationError(
+                    "Image size is too large! Please upload an image smaller than 2MB."
+                )
+            return image
+        else:
+            raise ValidationError("Couldn't read uploaded image")
